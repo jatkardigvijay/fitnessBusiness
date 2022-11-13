f5dc03ce87b5251653cce861c231eaeee850d0a0
snavin <mightymechnavi@gmail.com>
2022-11-13 17:25:00 +0000

    Added the images to the home page

--- html/js/data.js
+++ html/js/data.js
@@ -1,11 +1,11 @@
 const products = [
     {
         id: "dumbell",
-        name: "Dumbell - 10 kg (Sold in pairs only)",
-        description: "These are the authentic hex dumbells which are covered from all sides with hexagonal shaped rubber so that they do not roll even if fall. The grip, is made such that sweating won't cause any such problems like sliding from hands. They can be used for multiple body part workouts.",
+        name: "Dumbell - Primal Strength",
+        description: "The sizes 1kg to 30kg of the Primal Strength Rubber Hex Dumbbells are offered in pairs. The dumbbells have a lovely design that makes them both attractive in your gym and pleasant to use. To ensure optimal comfort, they have an easy-grip, ergonomic handle made of hardened chrome",
         price: "€65",
         image: [
-            "images/dumbells/dumbell10kg3.png",
+            "images/Equipment_Images/Dumbell/H_D.jpg",
             "images/dumbells/dumbell10kg1.png",
             "images/dumbells/dumbell10kg4.png",
             "images/dumbells/dumbell10kg5.png"
@@ -13,12 +13,12 @@ const products = [
 
     },
     {
-        id: "olympicbar",
-        name: "Olympic Bar - 20 kg",
-        description: "Particular for lifts/deadlifts until 100kgs, squats, barbell bench press. This bar is composed of high quality carbon steel. Collars are given free along with this bar.",
+        id: "barbell",
+        name: "Barbell - 20 kg",
+        description: "Barbells come in different weights depending on the goals and requirements of the activity 20kg weightlifting bars. This is because end users find this bar most helpful because they may use it for a variety of popular gym routines. Given that, it is the bar that is most frequently utilized on the gym floor. Despite the fact that 20kg is frequently used, weightlifting bars can be weighed at 10kg, 15kg, or 20kg for Olympic competition. ",
         price: "€90",
         image: [
-            "images/olympicbars/olympicbar2.png",
+            "images/Equipment_Images/Barbell/h.1_B.jpg",
             "images/olympicbars/olympicbar1.png",
             "images/olympicbars/olympicbar3.png",
             "images/olympicbars/olympicbar4.png"
@@ -31,7 +31,7 @@ const products = [
         description: "With this great freebody exercise, an astute quality Yoga mat gives a feeling of relaxtion and stress reduction. Moreover, it is easy for cleaning and doing the maintainance this mat.",
         price: "€15",
         image: [
-            "images/yogamats/yogamat1.png",
+            "images/Equipment_Images/Yogamat/h_Y.jpg",
             "images/yogamats/yogamat2.png",
             "images/yogamats/yogamat3.png",
             "images/yogamats/yogamat4.png"
@@ -39,12 +39,25 @@ const products = [
 
     },
     {
-        id: "indoorCyclingBike",
-        name: "G6 Indoor Cycling Bike",
-        description: "This indoor cycling bike makes your static cycling experience much better with the facilities it includes along with it. It is integrated with the IT system, making it more easy to save and track the fitness journey and steadily progress in the same along with speed and the calorie burned as the staple facilities being provided. The product also has 20% improved seating than the earlier one, which is in turn comfortable for the user.",
-        price: "€315",
+        id: "gymplates",
+        name: "Loaded Gym Plates – Premium",
+        description: "For an advanced muscle workout, plate loaded gym equipment from Injay offers unparalleled levels of safety and comfort, all while delivering impressive results. Plate loaded equipment such as selectorised curl and press machines are among the most effective strength building machines and perfectly complement strength workouts that require upper body training equipment. The Fitness Plate loaded Equipment is suitable for heavy commercial use and Home use.",
+        price: "€40",
         image: [
-            "images/indoorcyclingbike/indoorcyclingbike1.png",
+            "images/Equipment_Images/Plates/h_P.jpg",
+            "images/kettlebells/kettlebell2.png",
+            "images/kettlebells/kettlebell3.png",
+            "images/kettlebells/kettlebell4.png",
+        ]
+
+    },
+    {
+        id: "kettleBell",
+        name: "Kettle Bell - Premium-Cast",
+        description: "Most people agree that the Premium-Cast Kettlebells are the best premium-cast kettlebells currently on the market. The kettlebells are a need for any sports facility, gym, or CrossFit box, and their sturdy construction guarantees that they will last for years to come. These kettlebells have been expertly built using only premium-grade Iron Ore, not scrap iron (as with less expensive alternatives), and a one-piece cast mould guarantees the kettlebells' incredibly tough construction and flawless polish",
+        price: "€60",
+        image: [
+            "images/Equipment_Images/Kettlebell/h_K.jpg",
             "images/indoorcyclingbike/indoorcyclingbike2.png",
             "images/indoorcyclingbike/indoorcyclingbike3.png",
             "images/indoorcyclingbike/indoorcyclingbike4.png"
@@ -52,12 +65,24 @@ const products = [
 
     },
     {
-        id: "legpressmachine",
-        name: "Seated Leg Press",
-        description: "Standing or seating, the legs are always to be strong. Ideal leg press machine for the gym beginners, preparing for the next level leg workout (smith machine, Animal Plate Load Leg Press, etc.). The machine has a QR code(opens the application and has relevant videos) makes the user do the exercises without the need of trainer. Glutes, Quadriceps are the major muscles which are targetted.",
-        price: "€10,650",
+        id: "rowingMachine",
+        name: "Rowing Machine",
+        description: "Indoor rowing is the ideal form of training, suitable for any fitness level and any age, with regular use this machine will ensure you are fit and well throughout your life. Available to hire for the home and for commercial use. This robust piece of fitness equipment is low maintenance and easy to store or move around and comes with a digital monitor for accurate feedback, please note that we supply a PM5 or in some cases a PM3 monitor. If you need a specific monitor, please contact us after placing your order.",
+        price: "€50",
         image: [
-            "images/legpressmachine/legpress1.png",
+            "images/Equipment_Images/RowingMachine/h_RM.png",
+            "images/theraband/theraband2.png",
+            "images/theraband/theraband3.png",
+            "images/theraband/theraband4.png"
+        ]
+    },
+    {
+        id: "crossfitrope",
+        name: "Cross Fit Rope",
+        description: "The rope is made of 100% high-quality polyester.Most of the ropes available on the market are made of cheaper polypropylene or mixtures of derivatives, which results in less weight, worse flexibility and lower durability. It is the weight of the rope that directly affects the effectiveness of training, which is why this parameter should be taken into account when purchasing the ropes. The flexibility of the rope affects the comfort of training, high flexibility guarantees smooth operation of the rope during training and prevents it from twisting.",
+        price: "€75",
+        image: [
+            "images/Equipment_Images/CrossfitRope/f1_R.jpg",
             "images/legpressmachine/legpress2.png",
             "images/legpressmachine/legpress3.png",
             "images/legpressmachine/legpress4.png"
@@ -66,93 +91,18 @@ const products = [
 
     },
     {
-        id: "kettlebell",
-        name: "Kettlebell – 8 KG",
-        description: "This is high quality, ergonomically designed kettlebell available in variety of colours. It doesn't take a space and hence can be used at the gym and for home workout as well. The grip is designed much efficiently providing an ease to the hands after being lifted.",
-        price: "€40",
+        id: "treadmills",
+        name: "Treadmills",
+        description: "The Run attack, curved treadmill is designed for natural human movement. It’s modern concave-shape makes it very similar to running outside as it allows you to run with a natural style. The treadmill is powered solely by your legs, not electricity. It has no top speed and with its curved, slatted running surface, runners create a momentum to turn the treadmill belt by using the balls of their feet",
+        price: "€125",
         image: [
-            "images/kettlebells/kettlebell1.png",
-            "images/kettlebells/kettlebell2.png",
-            "images/kettlebells/kettlebell3.png",
-            "images/kettlebells/kettlebell4.png",
-        ]
-
-    },
-    {
-        id: "benchmachine",
-        name: "Weight Bench",
-        description: "This unique and adjustable bench can be used for varied amount of exercises, bench presses (incline, flat, decline). Machine is generally useful for home workout or a gym where number of people is small.",
-        price: "125",
-        image: [
-            "images/bench/bench1.png",
+            "images/Equipment_Images/Trademill/f1_T.png",
             "images/bench/bench2.png",
             "images/bench/bench3.png",
             "images/bench/bench4.png",
         ]
 
     },
-    {
-        id: "theraband",
-        name: "Thera Band",
-        description: "Thera bands is a one quick solution for many of the problems which includes physiotherapy amongst the main exercises. We can perofrm a wide range of exercises with therabands thus making it useful for home as well as gym i.e. both environments. One or more thera bands can be buyed according to the strength requirement.",
-        price: "€8 - €75",
-        image: [
-            "images/theraband/theraband1.png",
-            "images/theraband/theraband2.png",
-            "images/theraband/theraband3.png",
-            "images/theraband/theraband4.png"
-        ]
-    },
-    {
-        id: "benchmachine",
-        name: "Weight Bench",
-        description: "This unique and adjustable bench can be used for varied amount of exercises, bench presses (incline, flat, decline). Machine is generally useful for home workout or a gym where number of people is small.",
-        price: "125",
-        image: [
-            "images/bench/bench1.png",
-            "images/bench/bench2.png",
-            "images/bench/bench3.png",
-            "images/bench/bench4.png",
-        ]
-
-    },
-    {
-        id: "theraband",
-        name: "Thera Band",
-        description: "Thera bands is a one quick solution for many of the problems which includes physiotherapy amongst the main exercises. We can perofrm a wide range of exercises with therabands thus making it useful for home as well as gym i.e. both environments. One or more thera bands can be buyed according to the strength requirement.",
-        price: "€8 - €75",
-        image: [
-            "images/theraband/theraband1.png",
-            "images/theraband/theraband2.png",
-            "images/theraband/theraband3.png",
-            "images/theraband/theraband4.png"
-        ]
-    },
-    {
-        id: "benchmachine",
-        name: "Weight Bench",
-        description: "This unique and adjustable bench can be used for varied amount of exercises, bench presses (incline, flat, decline). Machine is generally useful for home workout or a gym where number of people is small.",
-        price: "125",
-        image: [
-            "images/bench/bench1.png",
-            "images/bench/bench2.png",
-            "images/bench/bench3.png",
-            "images/bench/bench4.png",
-        ]
-
-    },
-    {
-        id: "theraband",
-        name: "Thera Band",
-        description: "Thera bands is a one quick solution for many of the problems which includes physiotherapy amongst the main exercises. We can perofrm a wide range of exercises with therabands thus making it useful for home as well as gym i.e. both environments. One or more thera bands can be buyed according to the strength requirement.",
-        price: "€8 - €75",
-        image: [
-            "images/theraband/theraband1.png",
-            "images/theraband/theraband2.png",
-            "images/theraband/theraband3.png",
-            "images/theraband/theraband4.png"
-        ]
-    }
 ]
 
 //Display the details of the Products using JSON 
